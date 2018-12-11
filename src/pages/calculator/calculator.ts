287b8daee6ab623c7694f4d0427f8d20925a159c
shahin1290 <s.patowary@yahoo.com>
2018-12-11 01:17:20 +0100

    Adds two variables to hold the result, adds functionality to calculate BMI Add some markup on our template to display the results of our calculations

--- src/pages/calculator/calculator.ts
+++ src/pages/calculator/calculator.ts
@@ -16,11 +16,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class CalculatorPage {
   height: number;
   weight: number;
+  bmiValue: number;
+  bmiMessage: string;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   calculateBMI() {
-    // Add logic to calculate BMI here
+    if (this.weight > 0 && this.height > 0) {
+      let finalBmi = this.weight / (this.height / 100 * this.height / 100);
+      this.bmiValue = parseFloat(finalBmi.toFixed(2));
+      this.setBMIMessage();
+    }
+  }
+  
+  // setBMIMessage will set the text message based on the value of BMI
+  private setBMIMessage() {
+    if (this.bmiValue < 18.5) {
+      this.bmiMessage = "Underweight"
+    }
+  
+    if (this.bmiValue > 18.5 && this.bmiValue < 25) {
+      this.bmiMessage = "Normal"
+    }
+  
+    if (this.bmiValue > 25 && this.bmiValue < 30) {
+      this.bmiMessage = "Overweight"
+    }
+  
+    if (this.bmiValue > 30) {
+      this.bmiMessage = "Obese"
+    }
   }
 
 }
