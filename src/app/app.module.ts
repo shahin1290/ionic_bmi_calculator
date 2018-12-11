3ce4d3381481e4680c526aa56c1fbf70e051527c
shahin1290 <s.patowary@yahoo.com>
2018-12-11 01:01:04 +0100

    Generates calculator page, modifies app module and tabs to create calculator tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
+import { CalculatorPage } from '../pages/calculator/calculator'
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -13,6 +14,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     AboutPage,
+    CalculatorPage,
     TabsPage
   ],
   imports: [
@@ -23,6 +25,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     AboutPage,
+    CalculatorPage,
     TabsPage
   ],
   providers: [
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 
 import { AboutPage } from '../about/about';
+import { CalculatorPage } from '../calculator/calculator'
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
-
-  tab2Root = AboutPage;
-
+  calculatorTab = CalculatorPage; 
+  aboutTab = AboutPage;           
+ 
   constructor() {
 
   }
 }
+
