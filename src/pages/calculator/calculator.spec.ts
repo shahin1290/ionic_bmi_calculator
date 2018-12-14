caa000fbefc2fb7d693f9d54bf6854bf94a88639
shahin1290 <s.patowary@yahoo.com>
2018-12-14 23:12:13 +0100

    Adds spec for creating calculator page

---
+++ src/pages/calculator/calculator.spec.ts
@@ -0,0 +1,37 @@
+import { CalculatorPage } from "./calculator";
+import { TestBed, async } from "@angular/core/testing";
+import { IonicModule, Platform, NavController, NavParams } from "ionic-angular";
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { PlatformMock, StatusBarMock, SplashScreenMock, NavControllerMock, NavParamsMock } from "ionic-mocks";
+
+describe("CalculatorPage", () => {
+  let calculatorpage, fixture;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        CalculatorPage
+      ],
+      imports: [IonicModule.forRoot(CalculatorPage)],
+      providers: [
+        { provide: Platform, useFactory: () => PlatformMock.instance() },
+        { provide: StatusBar, useFactory: () => StatusBarMock.instance() },
+        { provide: SplashScreen, useFactory: () => SplashScreenMock.instance() },
+        { provide: NavController, useFactory: () => NavControllerMock.instance() },
+        { provide: NavParams, useFactory: () => NavParamsMock.instance() }
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CalculatorPage);
+    calculatorpage = fixture.componentInstance;
+  });
+
+  it("should create the calculator page", () => {
+    expect(calculatorpage).toBeTruthy();
+    expect(calculatorpage instanceof CalculatorPage).toEqual(true);
+  });
+
+});
