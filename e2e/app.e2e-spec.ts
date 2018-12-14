a4ff103e94f631bd7e7ae4472ae9e4930029ce32
shahin1290 <s.patowary@yahoo.com>
2018-12-14 20:04:31 +0100

    Adds e2e spec and logics to fill in form and shows the results

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -17,5 +17,13 @@ describe('App', () => {
         expect(title).toEqual('Ionic BMI Calculator');
       });
     });
+
+    it('fill in form', () => {
+      page.fillInForm(65, 165);
+      expect(page.results_card_header()).toContain('BMI Calculation');
+      expect(page.results_card_content()).toContain('BMI: 23.88');
+      expect(page.results_card_content()).toContain('You are Normal');
+    });
+
   })
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -13,4 +13,29 @@ export class Page {
  getTitleText() {
     return element(by.tagName('ion-title')).getText();
   }
+
+  fillInForm(weight, height) {
+
+    var list = element.all(by.css('.text-input'))
+
+    list.get(0).clear().then(() => {
+      list.get(0).sendKeys(weight);
+    })
+
+    list.get(1).clear().then(() => {
+      list.get(1).sendKeys(height);
+    })
+    
+    element(by.cssContainingText('.button-inner', 'Calculate')).click();
+   
+    browser.sleep(500);
+  }
+
+  results_card_header() {
+    return element(by.css('ion-card-header')).getText();
+  }
+
+  results_card_content() {
+    return element(by.css('ion-card-content')).getAttribute('textContent');
+  }
 }
